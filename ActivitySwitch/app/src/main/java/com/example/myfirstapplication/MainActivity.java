55b3f5d2e51f4a9b365391f8b641fde865a6a92b
YaroslavPr17 <yarikpristalov1702@yandex.ru>
2022-10-15 19:43:04 +0300

    Remove excess code

--- ActivitySwitch/app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ ActivitySwitch/app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     Button saverButton;
     EditText etName, etBirthday, etCourse;
     String name, dateBth, course;
-    private static final int SHOW_OTHER_ACTIVITY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,47 +47,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-//        Toast.makeText(getApplicationContext(), "Main onStart() called",
-//                Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-//        Toast.makeText(getApplicationContext(), "Main onRestart() called",
-//                Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-//        Toast.makeText(getApplicationContext(), "Main onPause() called",
-//                Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-//        Toast.makeText(getApplicationContext(), "Main onStop() called",
-//                Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-//        Toast.makeText(getApplicationContext(), "Main onResume() called",
-//                Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-//        Toast.makeText(getApplicationContext(), "Main onDestroy() called",
-//                Toast.LENGTH_LONG).show();
     }
 }
